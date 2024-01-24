53faf37f4e13b3bf4e99bbfbf63cdf6edd270519
Ilham Natsir <ilhamnatsirb@gmail.com>
2024-01-25 02:58:32 +0800

    fix menu tentang

--- app/src/main/java/com/hamtz/sistemkeamanan/ListActivity.kt
+++ app/src/main/java/com/hamtz/sistemkeamanan/ListActivity.kt
@@ -1,11 +1,13 @@
 package com.hamtz.sistemkeamanan
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 
 class ListActivity : AppCompatActivity() {
     private lateinit var webView: WebView
@@ -14,13 +16,22 @@ class ListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
         webView = findViewById(R.id.listWebview)
-        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                // Menggunakan WebViewClient untuk menangani permintaan URL
+                return false
+            }
+        }
+
 
         // this will load the url of the website
 //        webView.loadUrl("https://mamanialaundry.my.id/list-deteksi")
         webView.loadUrl("https://myshared.my.id/list-deteksi")
-        // this will enable the javascript settings, it can also allow xss vulnerabilities
-        webView.settings.javaScriptEnabled = true
 
         webView.setWebChromeClient(WebChromeClient())
 
@@ -28,13 +39,29 @@ class ListActivity : AppCompatActivity() {
         webView.settings.setSupportZoom(true)
     }
 
-    override fun onBackPressed() {
-        // if your webview can go back it will go back
-        if (webView.canGoBack())
-            webView.goBack()
-        // if your webview cannot go back
-        // it will exit the application
-        else
-            super.onBackPressed()
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (event.getAction() === KeyEvent.ACTION_DOWN) {
+            when (keyCode) {
+                KeyEvent.KEYCODE_BACK -> {
+                    if (webView.canGoBack()) {
+                        webView.goBack()
+                    } else {
+                        finish()
+                    }
+                    return true
+                }
+            }
+        }
+        return super.onKeyDown(keyCode, event)
     }
+
+//    override fun onBackPressed() {
+//        // if your webview can go back it will go back
+//        if (webView.canGoBack())
+//            webView.goBack()
+//        // if your webview cannot go back
+//        // it will exit the application
+//        else
+//            super.onBackPressed()
+//    }
 }
